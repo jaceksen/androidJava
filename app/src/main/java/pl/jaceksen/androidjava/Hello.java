c9b6d04a4460ad2b3b6627cb73a0b9e8b018c851
jaceksen <jaceksen@gmail.com>
2017-01-20 11:57:46 +0100

    pierwszy commit

---
+++ app/src/main/java/pl/jaceksen/androidjava/Hello.java
@@ -0,0 +1,14 @@
+package pl.jaceksen.androidjava;
+
+/**
+ * Created by jsen on 20.01.17.
+ */
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Ala ma kota.");
+        System.out.println("Ola ma kota");
+    }
+
+}
