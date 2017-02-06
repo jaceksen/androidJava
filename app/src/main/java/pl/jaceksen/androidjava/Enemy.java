f5d9aa24ad46ee808bd8c3f38f630c6e01fe2ca3
Jacek Seń <jaceksen@gmail.com>
2017-02-06 18:33:10 +0100

    170206

---
+++ app/src/main/java/pl/jaceksen/androidjava/Enemy.java
@@ -0,0 +1,42 @@
+package pl.jaceksen.androidjava;
+
+/**
+ * Created by jsen on 06.02.17.
+ */
+
+public class Enemy {
+
+    private String name;
+    private int hitPoints;
+    private int lives;
+
+    public Enemy(String name, int hitPoints, int lives) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.lives = lives;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+}
